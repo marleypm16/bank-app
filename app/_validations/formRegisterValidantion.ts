83d71b30690a3cd208fc902ea1ea234e40f01195
marleypm16 <yarlempm16@gmail.com>
2024-07-05 09:31:42 -0300

    feat add form validation

---
+++ app/_validations/formRegisterValidantion.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const formRegisterValidation = z.object({
+    email: z.string().email(),
+    password: z.string({
+        required_error: "Password is required",
+    }).min(8, "Password must be at least 8 characters"),
+    firstName: z.string().min(1, "First name is required"),
+    lastName: z.string().min(1, "Last name is required"),
+    address: z.string().min(1, "Address is required"),
+    state: z.string().min(1, "State is required"),
+    zipCode: z.string().min(1, "Zip code is required"),
+    dateOfBirth: z.string().min(1, "Date of birth is required"),
+});
+
+export type FormRegisterValidationType = z.infer<typeof formRegisterValidation>;
