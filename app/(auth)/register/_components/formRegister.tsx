293e6ebd0aa2d8ade670a4c7a8a18e4ad7c4f68e
marleypm16 <yarlempm16@gmail.com>
2024-07-05 10:39:28 -0300

    feat : add loading and calendar component

--- app/(auth)/register/_components/formRegister.tsx
+++ app/(auth)/register/_components/formRegister.tsx
@@ -5,10 +5,18 @@ import { formRegisterValidation, FormRegisterValidationType } from "@/app/_valid
 import { useForm } from "react-hook-form";
 import {zodResolver} from '@hookform/resolvers/zod';
 import { Button } from "@/app/_components/ui/button";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { cepApi } from "@/app/_lib/cepApi";
 import SelectState from "./selectState";
+import { createUser } from "@/app/_lib/createUser";
+import { SignUpParams } from "@/app/_types/signUpParams";
+import { Popover, PopoverContent, PopoverTrigger } from "@/app/_components/ui/popover";
+import { Calendar } from "@/app/_components/ui/calendar";
+import { CalendarIcon, Loader2 } from "lucide-react";
+import { format } from "date-fns";
+import { redirect } from "next/navigation";
 const FormRegister = () => {
+    const [loading, setLoading] = useState(false)
     const form = useForm<FormRegisterValidationType>({
         resolver: zodResolver(formRegisterValidation),
         defaultValues: {
@@ -19,7 +27,6 @@ const FormRegister = () => {
             address: "",
             state: "",
             zipCode: "",
-            dateOfBirth: "",
         }
     })
     const {getValues,setValue} = form
@@ -34,8 +41,13 @@ const FormRegister = () => {
     const handleSelect = (value: string) => {
         setValue('state',value)
     }
-    const onSubmit = (data: FormRegisterValidationType) => {
-        console.log(data);
+    const onSubmit = async (data: SignUpParams) => {
+        console.log(data)
+        setLoading(true)
+        await createUser(data)
+        setLoading(false)
+        form.reset()
+        redirect("/login")
     }
     return(
         <Form {...form}>
@@ -110,18 +122,42 @@ const FormRegister = () => {
                 />
             </div>
             <FormField
-                control={form.control}
-                name="dateOfBirth"
-                render={({ field }) => (
-                <FormItem>
-                    <FormLabel>Date Of Birth</FormLabel>
+            control={form.control}
+            name="dateOfBirth"
+            render={({ field }) => (
+                <FormItem className="flex flex-col">
+                <FormLabel>Date of birth</FormLabel>
+                <Popover>
+                    <PopoverTrigger asChild>
                     <FormControl>
-                    <Input type="date" {...field} />
+                        <Button
+                        variant={"outline"}
+                        >
+                        {field.value ? (
+                            format(field.value, "PPP")
+                        ) : (
+                            <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
                     </FormControl>
-                    <FormMessage />
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                        }
+                        initialFocus
+                    />
+                    </PopoverContent>
+                </Popover>
+                <FormMessage />
                 </FormItem>
-                )}
-            />
+            )}
+        />
             <FormField
                 control={form.control}
                 name="email"
@@ -150,7 +186,11 @@ const FormRegister = () => {
             />
         
           
-          <Button type="submit">Submit</Button>
+          <Button type="submit">{
+                loading ? (
+                    <p>Loading <Loader2 className="spin"/></p>
+                ) : "Submit"
+            }</Button>
         </form>
       </Form>
     )
