d83b68c469b95e371f48032da6212ccac023165e
marleypm16 <yarlempm16@gmail.com>
2024-07-05 10:40:17 -0300

    feat : signUpParams type

---
+++ app/_types/signUpParams.ts
@@ -0,0 +1,10 @@
+export type SignUpParams = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    dateOfBirth: Date;
+    zipCode: string;
+    state: string;
+    address: string;
+}
