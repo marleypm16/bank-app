7fd1c5f8593360ef5c1fbdde344f508f66e0c882
marleypm16 <yarlempm16@gmail.com>
2024-07-05 09:33:00 -0300

    feat : add register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logo from "../../_assets/imgs/logo_app.svg"
 import FormRegister from "./_components/formRegister";
+import imagePreview from "../../_assets/imgs/imagePreview.svg";
 const RegisterPage = () => {
     return(
         <div className="h-screen">
@@ -9,7 +10,9 @@ const RegisterPage = () => {
                 <h1 className="text-2xl font-bold">SignUp</h1>
                 <FormRegister/>
             </div>
-            <div className="w-1/2"></div>
+            <div className="w-1/2">
+                <Image src={imagePreview} alt="app preview" height={500} width={500}/>
+            </div>
         </div>
     )
 }
