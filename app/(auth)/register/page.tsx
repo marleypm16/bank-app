272cdad48e6a98e51ee7f634dd0dc76530c252c8
marleypm16 <yarlempm16@gmail.com>
2024-07-05 09:59:57 -0300

    refactor : import image preview

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import logo from "../../_assets/imgs/logo_app.svg"
 import FormRegister from "./_components/formRegister";
-import imagePreview from "../../_assets/imgs/imagePreview.svg";
+import imagePreview from "../../_assets/imgs/image_preview.svg";
 const RegisterPage = () => {
     return(
-        <div className="h-screen">
-            <div className="w-1/2">
+        <div className="max-h-screen flex items-center justify-between">
+            <div className="w-1/2 h-1/2">
                 <Image src={logo} alt="Horizon bank" height={200} width={200}/>
                 <h1 className="text-2xl font-bold">SignUp</h1>
                 <FormRegister/>
